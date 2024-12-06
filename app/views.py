27eda07b6002fc63cb81cab987732a3d1adf1854
Mert Ozkaya <ozkayamert35@gmail.com>
2024-12-06 14:44:17 +0300

    Migrations, models, add product

--- app/views.py
+++ app/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.views import View
+from .models import Product
 
 def home(request):
     return render(request, "app/home.html")
+
+class CategoryView(View):
+    def get(self, request, val):
+        product = Product.objects.filter(category=val)
+        return render(request, "app/category.html", locals())
